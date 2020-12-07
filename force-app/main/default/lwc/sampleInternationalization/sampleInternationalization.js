3ba1eb6ab8c3063b249f5e3955ba8b54dc326ba2
Andrew Poblocki <apoblocki@salesforce.com>
2020-12-07 15:21:32 -0800

    Adding non-Internationalized code

---
+++ force-app/main/default/lwc/sampleInternationalization/sampleInternationalization.js
@@ -0,0 +1,23 @@
+import { LightningElement } from 'lwc';
+
+// Import custom labels
+import greeting from '@salesforce/label/c.greeting';
+import salesforceLogoDescription from '@salesforce/label/c.salesforceLogoDescription';
+
+export default class SampleInternationalization extends LightningElement {
+    // Expose the static resource URL to use in the template.
+    logoUrl =
+        'https://developer.salesforce.com/resources2/images/salesforce-header-logo.png';
+
+    // Expose the labels to use in the template.
+    label = {
+        greeting,
+        salesforceLogoDescription
+    };
+
+    get currentDate() {
+        var date = new Date();
+
+        return new Intl.DateTimeFormat().format(date);
+    }
+}
